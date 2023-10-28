67fdc0d1b69ed356ccae5c3cd438ad54362154a4
Namhee <padoingg@gmail.com>
2023-10-29 01:12:10 +0900

    update: main ui 구성

--- src/ui.py
+++ src/ui.py
@@ -47,14 +47,112 @@ class FletUiMainscreen():
         global g_ui_instance;
 
         if g_ui_instance != None and g_ui_instance._ui_ft != None:
-            g_ui_instance._ui_ft.app(target=g_ui_instance._ui_mainscreen.fn_flet_main);
+            g_ui_instance._ui_ft.app(target=g_ui_instance._ui_mainscreen.fn_flet_main, view=ft.WEB_BROWSER);
     
     def fn_end(self): pass;
     def fn_enable(self): pass;
     def fn_disable(self): pass;
 
-    def fn_flet_main(self, page): 
-        page.add(g_ui_instance._ui_ft.Text(value="Hello, world!"));
+    def fn_flet_main(self, page):
+        def view_pop(e):
+            page.views.pop()
+            top_view = page.views[-1]
+            page.go(top_view.route)
+
+        def send_message_click(e):
+            page.go("/result")
+
+        def route_change(e):
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    "/",
+                    [
+                        ft.Container(
+                            content=ft.Text(
+                                "AutoMedic",
+                                size=50,
+                                color='white',
+                                font_family='poppins',
+                                weight=ft.FontWeight.BOLD,
+                            ),
+                            bgcolor=ft.colors.SURFACE_VARIANT,
+                            alignment=ft.alignment.center,
+                            border_radius=5,
+                            padding=5,
+                            expand=True,
+                        ),
+                        ft.Row(
+                            [
+                                ft.TextField(
+                                    hint_text="증상을 입력하세요",
+                                    autofocus=True,
+                                    shift_enter=True,
+                                    min_lines=1,
+                                    max_lines=5,
+                                    filled=True,
+                                    border=ft.InputBorder.NONE,
+                                    bgcolor=ft.colors.SURFACE_VARIANT,
+                                    expand=True,
+                                    #on_submit=send_message_click,
+                                ),
+                                ft.IconButton(
+                                    icon=ft.icons.SEND_ROUNDED,
+                                    tooltip="검색 결과 확인",
+                                    on_click=send_message_click,
+                                ),
+                            ]
+                        )
+                    ],
+                )
+            )
+            if page.route == "/result":
+                page.views.append(
+                    ft.View(
+                        "/result",
+                        [
+                            ft.Row(
+                                [
+                                    ft.Container(
+                                        content=ft.Text(
+                                            "panel area", 
+                                            size=20, 
+                                            color='black', 
+                                            font_family='poppins', 
+                                            weight=ft.FontWeight.BOLD
+                                        ),
+                                        alignment=ft.alignment.center,
+                                        bgcolor=ft.colors.SURFACE_VARIANT,
+                                        border_radius=5,
+                                        padding=10,
+                                        height=page.height,
+                                        expand=2,
+                                    ),
+                                    ft.Container(
+                                        content=ft.Text(
+                                            "map area", 
+                                            size=20, 
+                                            color='black', 
+                                            font_family='poppins', 
+                                            weight=ft.FontWeight.BOLD
+                                        ),
+                                        alignment=ft.alignment.center,
+                                        bgcolor=ft.colors.SURFACE_VARIANT,
+                                        border_radius=5,
+                                        padding=10,
+                                        height=page.height,
+                                        expand=3,
+                                    )
+                                ]
+                            )
+                        ],
+                    )
+                )
+            page.update()
+
+        page.on_route_change = route_change
+        page.on_view_pop = view_pop
+        page.go(page.route)
 ##################################################
 class FletUiSearchbar():
     def __init__(self): pass;
